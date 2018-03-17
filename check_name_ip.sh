e06389b3b7c9f7b81a8a249f8da4d8394fd56c03
Operador <operador@dcconsultores.es>
2018-03-17 14:19:05 +0100

    Adaptamos el script para EC2..

--- check_name_ip.sh
+++ check_name_ip.sh
@@ -2,6 +2,6 @@
 echo -e "\n\nEl nombre del servidor es:";
 echo -e "\n$(hostname)";
 echo -e "\n\nLas direcciones IP son:"
-echo -e "\n$(sudo ifconfig | awk '/inet:/{print substr($2,6)}')";
+echo -e "\n$(ifconfig | awk '/inet addr:/{print substr($2,6)}')";
 echo
 exit 0;
